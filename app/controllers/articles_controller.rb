019c95d9873eaa077ea947d9053a7895c8cee4fb
Rails Udemy Course <railsudemycourse@gmail.com>
2016-12-09 03:49:36 -0500

    Add partials and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,16 +21,25 @@ class ArticlesController < ApplicationController
   
   def update
     @article = Article.find(params[:id])
-    if @article.update
+    if @article.update(article_params)
       flash[:notice] = "Article was susscessfully updated"
       redirect_to article_path(@article)
      else
       render "edit"
     end
   end
-def show
-  @article = Article.find(params[:id])
-end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was susscessfully deleted"
+    redirect_to articles_path
+  end
+  
   private
   def article_params
     params.require(:article).permit(:title, :description)
