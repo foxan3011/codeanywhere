4cd80e74bec50f2ffb4f0e94769d26bf55cb9048
DATDANG <foxan3011@gmail.com>
2017-01-17 08:49:05 +0000

    One to many association

--- Gemfile
+++ Gemfile
@@ -37,7 +37,6 @@ gem 'spring',        group: :development
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'byebug'
 
-# Use debugger
-# gem 'debugger', group: [:development, :test]
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,14 +13,16 @@ class ArticlesController < ApplicationController
   end
   
   def create
+
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
     end
-   end
+  end
   
   def update
     if @article.update(article_params)
