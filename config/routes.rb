f0a328359189e7b24f7374dc9c97042ec8aa7468
DATDANG <foxan3011@gmail.com>
2017-01-18 08:40:09 +0000

    New User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
 
 root 'pages#home'
 get 'about', to: 'pages#about' 
+
 resources :articles
+
+get 'signup', to: 'users#new'
+
+resources :users, except: [:new] 
   
   
   # Example of regular route:
