ef26cb0d07f5e52657b4fe1bfb9512bb63f1776e
DATDANG <foxan3011@gmail.com>
2017-01-20 10:45:37 +0000

    Authentication Methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    
+    before_action :set_user, only: [:edit, :update, :show]
     def index
        @users = User.paginate(page: params[:page], per_page: 5)
     end
@@ -23,10 +23,10 @@ class UsersController < ApplicationController
     
     
     def edit
-        @user = User.find(params[:id])
+        
     end
     def update
-        @user = User.find(params[:id])
+        
         if  @user.update(user_params)
             flash[:success] = "Your account was updated successfully"
             redirect_to users_path
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     end
     
     def show
-       @user = User.find(params[:id])
+      
        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
@@ -46,4 +46,9 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+    
+    def set_user
+        @user = User.find(params[:id])
+    end
+
 end
