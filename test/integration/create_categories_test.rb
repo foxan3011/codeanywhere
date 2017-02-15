29a2cba49b607c9fd345845868093dcc4cf70295
DATDANG <foxan3011@gmail.com>
2017-02-15 03:56:37 +0000

    Finish UI Categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -6,14 +6,12 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
     test "get new category form and create category" do
         sign_in_as(@user, "password")
-        
-        
         get new_category_path
         assert_template 'categories/new'
     #being able to create a category and then display it sw    
         assert_difference 'Category.count', 1 do
     #should be a difference in category count of one after do this action
-            post_via_redirect categories_path, category: {name: "sports"}
+        post_via_redirect categories_path, category: {name: "sports"}
     #remember the submission of a new form is handled by the create action which is http Post request
     #HTTP post request to the category's path which gets reacted to the create action
         end
@@ -23,14 +21,13 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     
     test "invalid category submission results in failure"   do
         sign_in_as(@user, "password")
-   
         get new_category_path
         assert_template 'categories/new'
         assert_no_difference 'Category.count', 1 do
         post categories_path, category: {name: " "}
-    end
+        end
         assert_template 'categories/new'
         assert_select 'h2.panel-title'
         assert_select 'div.panel-body' #errors.html.erb
- end
+    end
  end
